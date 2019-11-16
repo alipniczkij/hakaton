3458a599fd76339ffd233ea46cb6159aeaf1ce07
Diroman <roman.dubatov@gmail.com>
2019-11-16 19:32:52 +0300

    keyboard

--- vk_bot.py
+++ vk_bot.py
@@ -1,5 +1,7 @@
 import vk_api
 import psycopg2
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 from vk_api.longpoll import VkLongPoll, VkEventType
 from classes import VkBot
 import random
@@ -7,26 +9,27 @@ import random
 conn = psycopg2.connect(database="hack", user="postgres", host="100.100.153.231",
                         port="5432", password="verepa40")
 cursor = conn.cursor()
+keyboard = None
 
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
 
 
 token = '9becfa2210b0bbd028f47951ae253aa88c15fc26c6c938578980bdd472581fcaad29eab2afd0f7109f06f'
-
-
 vk = vk_api.VkApi(token=token)
-
+vk_mess = vk.get_api()
+bot = VkBot(event.user_id)
 
 longpoll = VkLongPoll(vk)
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
-            print('New message:')
             print(f'For me by: {event.user_id}', end='')
 
-            bot = VkBot(event.user_id)
-            write_msg(event.user_id, bot.new_message(event.text))
-
+            create_keyboard()
+            vk_mess.messages.send(
+                peer_id=event.user_id,
+                random_id=get_random_id(),
+                keyboard=keyboard.get_keyboard(),
+                message='Пример клавиатуры'
+            )
             print('Text: ', event.text)
