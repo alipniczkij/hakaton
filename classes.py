30aa5ffc71905a5abc25069e7eec89c512e473f5
Diroman <roman.dubatov@gmail.com>
2019-11-16 20:59:00 +0300

    work

--- classes.py
+++ classes.py
@@ -1,10 +1,15 @@
+from db import session, User, Message
+from bot_neuro import *
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 import requests
+import json
 import bs4
 
 
-def create_start_keyboard():
-    keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button('Регистрация', color=VkKeyboardColor.POSITIVE)
+def create_start_keyboard(text):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button(text, color=VkKeyboardColor.POSITIVE)
     return keyboard
     # keyboard.add_button('Белая кнопка', color=VkKeyboardColor.DEFAULT)
     # keyboard.add_button('Зелёная кнопка', color=VkKeyboardColor.POSITIVE)
@@ -17,9 +22,9 @@ def create_start_keyboard():
 class VkBot:
     def __init__(self, vk):
         self.vk = vk
-        self.start_keyboard = create_start_keyboard()
-        self._USERNAME = self._get_user_name_from_vk_id(user_id)
-        self._COMMANDS = ["РЕГИСТРАЦИЯ"]
+        self.start_keyboard = create_start_keyboard('Регистрация')
+        # self._USERNAME = self._get_user_name_from_vk_id(user_id)
+        self._COMMANDS = ["РЕГИСТРАЦИЯ", "ВАФЛЯ"]
 
     def _get_user_name_from_vk_id(self, user_id):
         request = requests.get("https://vk.com/id" + str(user_id))
@@ -50,16 +55,47 @@ class VkBot:
 
         return result
 
-    def new_message(self, user_id, message):
-        if message.upper() == self._COMMANDS[0]:
-            return f"Привет-привет, {self._USERNAME}!"
+    def login_user(self, user_id):
+        in_system = session.query(User).filter_by(user_id=user_id).first()
+        if in_system:
+            self.send_message(user_id=user_id, messages="Введите запрос:")
         else:
-            return "Не понимаю о чем вы..."
+            user = User(user_id)
+            session.add(user)
+            session.commit()
+
+    def save_massage(self, user_id, message):
+        mes = Message(user_id, message)
+        session.add(mes)
+        session.commit()
+
+    def new_message(self, user_id, message):
+
+        in_system = session.query(User).filter_by(user_id=user_id).first()
+        if not in_system and message.upper() != self._COMMANDS[0] and message.upper() != self._COMMANDS[1]:
+            self.send_message(user_id, messages='Требуется регистрация', keyboard=self.start_keyboard)
+            return
+        self.save_massage(user_id, message)
+
+        if message.upper() == self._COMMANDS[0]:
+            self.send_message(user_id=user_id, messages="Введите кодовое слово")
+        elif message.upper() == self._COMMANDS[1]:
+            self.login_user(user_id)
+        else:
+            answer = classify(message)
+            self.send_message(messages='Класс - ' + str(answer[0]), user_id=user_id)
 
     def send_message(self, user_id, messages='', keyboard=None):
-        self_vk.messages.send(
-            peer_id=user_id,
-            random_id=get_random_id(),
-            keyboard=keyboard,
-            message=messages
-        )
+        if keyboard:
+            self.vk.messages.send(
+                peer_id=user_id,
+                random_id=get_random_id(),
+                keyboard=json.dumps(keyboard.keyboard),
+                message=messages
+            )
+        else:
+            self.vk.messages.send(
+                peer_id=user_id,
+                random_id=get_random_id(),
+                message=messages
+            )
--- vk_bot.py
+++ vk_bot.py
@@ -1,16 +1,11 @@
 import vk_api
-import psycopg2
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from vk_api.utils import get_random_id
 from vk_api.longpoll import VkLongPoll, VkEventType
 from classes import VkBot
 import random
 
-conn = psycopg2.connect(database="hack", user="postgres", host="100.100.153.231",
-                        port="5432", password="verepa40")
-cursor = conn.cursor()
-keyboard = None
 
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///:memory:', echo=True)
 
 token = '9becfa2210b0bbd028f47951ae253aa88c15fc26c6c938578980bdd472581fcaad29eab2afd0f7109f06f'
 vk = vk_api.VkApi(token=token)
@@ -22,5 +17,5 @@ longpoll = VkLongPoll(vk)
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
-            bot.new_messages(event.user_id, event.text)
+            bot.new_message(event.user_id, event.text)
             print('Text: ', event.text)
